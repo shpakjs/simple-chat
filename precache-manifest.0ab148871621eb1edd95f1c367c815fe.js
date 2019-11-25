8424be13d3f05d0b86c638403b93f81c16397eda
Iuliia Shpak <iushp@smart-it.com>
2019-11-25 18:31:11 +0200

    Updates

---
+++ precache-manifest.0ab148871621eb1edd95f1c367c815fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c2a68e16461e01db161c0c64e8287b",
+    "url": "/simple-chat/index.html"
+  },
+  {
+    "revision": "c65eb9804e29c8163855",
+    "url": "/simple-chat/static/css/main.329b6e3a.chunk.css"
+  },
+  {
+    "revision": "145217d8f55621472269",
+    "url": "/simple-chat/static/js/2.26c632c1.chunk.js"
+  },
+  {
+    "revision": "c65eb9804e29c8163855",
+    "url": "/simple-chat/static/js/main.08d7bb65.chunk.js"
+  },
+  {
+    "revision": "fd61ef5ca6e844a152b3",
+    "url": "/simple-chat/static/js/runtime-main.bfb5af26.js"
+  }
+]);
